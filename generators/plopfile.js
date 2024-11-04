8a74b8717189fa4bfc8ac2995118c794c7af6d92
Túlio <tulio.lira@outlook.com>
2024-11-03 23:17:29 -0300

    changed plopfile 'name' to 'model'

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -5,29 +5,29 @@ module.exports = (plop) => {
     prompts: [
       {
         type: 'input',
-        name: 'name',
+        name: 'model',
         message: 'Digite o nome do model (ex. User, Aviso):',
       },
     ],
     actions: [
       {
         type: 'add',
-        path: '../src/controllers/{{pascalCase name}}Controller.ts',
+        path: '../src/controllers/{{pascalCase model}}Controller.ts',
         templateFile: 'templates/singleId/regularController.ts.hbs',
       },
       {
         type: 'add',
-        path: '../src/repositories/{{pascalCase name}}Repository.ts',
+        path: '../src/repositories/{{pascalCase model}}Repository.ts',
         templateFile: 'templates/singleId/regularRepository.ts.hbs',
       },
       {
         type: 'add',
-        path: '../src/routes/{{pascalCase name}}Routes.ts',
+        path: '../src/routes/{{pascalCase model}}Routes.ts',
         templateFile: 'templates/singleId/regularRoutes.ts.hbs',
       },
       {
         type: 'add',
-        path: '../src/DTOs/{{pascalCase name}}.ts',
+        path: '../src/DTOs/{{pascalCase model}}.ts',
         templateFile: 'templates/singleId/regularDTO.ts.hbs',
       },
       {
@@ -35,40 +35,40 @@ module.exports = (plop) => {
         path: '../src/DTOs/index.ts',
         pattern: /^(import .*;\n)/,
         template:
-          "import { {{pascalCase name}}, Update{{pascalCase name}} } from './{{pascalCase name}}';\n$1",
+          "import { {{pascalCase model}}, Update{{pascalCase model}} } from './{{pascalCase model}}';\n$1",
       },
       {
         type: 'modify',
         path: '../src/controllers/index.ts',
         pattern: /(export {)/,
         template:
-          "import {{pascalCase name}}Controller from './{{pascalCase name}}Controller';\n\n$1",
+          "import {{pascalCase model}}Controller from './{{pascalCase model}}Controller';\n\n$1",
       },
       {
         type: 'modify',
         path: '../src/repositories/index.ts',
         pattern: /(export {)/,
         template:
-          "import {{pascalCase name}}Repository from './{{pascalCase name}}Repository';\n\n$1",
+          "import {{pascalCase model}}Repository from './{{pascalCase model}}Repository';\n\n$1",
       },
 
       {
         type: 'append',
         path: '../src/controllers/index.ts',
-        template: 'export { {{pascalCase name}}Controller };\n',
+        template: 'export { {{pascalCase model}}Controller };\n',
         skipIfExists: true,
       },
       {
         type: 'append',
         path: '../src/repositories/index.ts',
-        template: 'export { {{pascalCase name}}Repository };\n',
+        template: 'export { {{pascalCase model}}Repository };\n',
         skipIfExists: true,
       },
       {
         type: 'append',
         path: '../src/DTOs/index.ts',
         template:
-          'export { {{pascalCase name}}, Update{{pascalCase name}} };\n',
+          'export { {{pascalCase model}}, Update{{pascalCase model}} };\n',
         skipIfExists: true,
       },
       {
@@ -76,14 +76,14 @@ module.exports = (plop) => {
         path: '../src/routes/index.ts', // O mesmo arquivo
         pattern: /\n/, // Localização específica
         template:
-          "\n\nimport {{pascalCase name}}Routes from './{{pascalCase name}}Routes';",
+          "\n\nimport {{pascalCase model}}Routes from './{{pascalCase model}}Routes';",
       },
       {
         type: 'modify',
         path: '../src/routes/index.ts', // O mesmo arquivo
         pattern: /const router = Router\(\);/, // Padrão para encontrar a linha do router.use
         template:
-          "const router = Router();\n\nrouter.use('/{{camelCase name}}', {{pascalCase name}}Routes);",
+          "const router = Router();\n\nrouter.use('/{{camelCase model}}', {{pascalCase model}}Routes);",
       },
     ],
   });
