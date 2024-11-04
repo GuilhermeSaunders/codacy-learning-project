0594c8171dfe12a76f199d839d0b5b6aa649078e
Túlio <tulio.lira@outlook.com>
2024-11-03 23:18:02 -0300

    added doubleId option

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,5 +1,5 @@
 module.exports = (plop) => {
-  // Gerador para controlador, repositório, rotas e DTO
+  // Gerador para controlador, repositório, rotas e DTO com identificador único ou duplo
   plop.setGenerator('model', {
     description: 'Create a controller, repository, routes, and DTO for a model',
     prompts: [
@@ -8,83 +8,189 @@ module.exports = (plop) => {
         name: 'model',
         message: 'Digite o nome do model (ex. User, Aviso):',
       },
+      {
+        type: 'list',
+        name: 'identifierType',
+        message: 'O modelo possui identificador único ou duplo?',
+        choices: ['Único', 'Duplo'],
+      },
+      {
+        type: 'input',
+        name: 'model2',
+        message: 'Digite o nome do segundo model (ex. Product):',
+        when: (answers) => answers.identifierType === 'Duplo',
+      },
     ],
-    actions: [
-      {
-        type: 'add',
-        path: '../src/controllers/{{pascalCase model}}Controller.ts',
-        templateFile: 'templates/singleId/regularController.ts.hbs',
-      },
-      {
-        type: 'add',
-        path: '../src/repositories/{{pascalCase model}}Repository.ts',
-        templateFile: 'templates/singleId/regularRepository.ts.hbs',
-      },
-      {
-        type: 'add',
-        path: '../src/routes/{{pascalCase model}}Routes.ts',
-        templateFile: 'templates/singleId/regularRoutes.ts.hbs',
-      },
-      {
-        type: 'add',
-        path: '../src/DTOs/{{pascalCase model}}.ts',
-        templateFile: 'templates/singleId/regularDTO.ts.hbs',
-      },
-      {
-        type: 'modify',
-        path: '../src/DTOs/index.ts',
-        pattern: /^(import .*;\n)/,
-        template:
-          "import { {{pascalCase model}}, Update{{pascalCase model}} } from './{{pascalCase model}}';\n$1",
-      },
-      {
-        type: 'modify',
-        path: '../src/controllers/index.ts',
-        pattern: /(export {)/,
-        template:
-          "import {{pascalCase model}}Controller from './{{pascalCase model}}Controller';\n\n$1",
-      },
-      {
-        type: 'modify',
-        path: '../src/repositories/index.ts',
-        pattern: /(export {)/,
-        template:
-          "import {{pascalCase model}}Repository from './{{pascalCase model}}Repository';\n\n$1",
-      },
+    actions: (data) => {
+      const actions = [];
 
-      {
-        type: 'append',
-        path: '../src/controllers/index.ts',
-        template: 'export { {{pascalCase model}}Controller };\n',
-        skipIfExists: true,
-      },
-      {
-        type: 'append',
-        path: '../src/repositories/index.ts',
-        template: 'export { {{pascalCase model}}Repository };\n',
-        skipIfExists: true,
-      },
-      {
-        type: 'append',
-        path: '../src/DTOs/index.ts',
-        template:
-          'export { {{pascalCase model}}, Update{{pascalCase model}} };\n',
-        skipIfExists: true,
-      },
-      {
-        type: 'modify',
-        path: '../src/routes/index.ts', // O mesmo arquivo
-        pattern: /\n/, // Localização específica
-        template:
-          "\n\nimport {{pascalCase model}}Routes from './{{pascalCase model}}Routes';",
-      },
-      {
-        type: 'modify',
-        path: '../src/routes/index.ts', // O mesmo arquivo
-        pattern: /const router = Router\(\);/, // Padrão para encontrar a linha do router.use
-        template:
-          "const router = Router();\n\nrouter.use('/{{camelCase model}}', {{pascalCase model}}Routes);",
-      },
-    ],
+      // Escolher o template correto de acordo com o tipo de identificador
+      const templatePath =
+        data.identifierType === 'Duplo'
+          ? 'templates/doubleId'
+          : 'templates/singleId';
+
+      // Ações para controlador, repositório, rotas e DTO
+      actions.push(
+        {
+          type: 'add',
+          path: `../src/controllers/{{pascalCase model}}Controller.ts`,
+          templateFile: `${templatePath}/regularController.ts.hbs`,
+        },
+        {
+          type: 'add',
+          path: `../src/repositories/{{pascalCase model}}Repository.ts`,
+          templateFile: `${templatePath}/regularRepository.ts.hbs`,
+        },
+        {
+          type: 'add',
+          path: `../src/routes/{{pascalCase model}}Routes.ts`,
+          templateFile: `${templatePath}/regularRoutes.ts.hbs`,
+        },
+        {
+          type: 'add',
+          path: `../src/DTOs/{{pascalCase model}}.ts`,
+          templateFile: `${templatePath}/regularDTO.ts.hbs`,
+        },
+        {
+          type: 'modify',
+          path: '../src/DTOs/index.ts',
+          pattern: /^(import .*;\n)/,
+          template:
+            "import { {{pascalCase model}}, Update{{pascalCase model}} } from './{{pascalCase model}}';\n$1",
+        },
+        {
+          type: 'modify',
+          path: '../src/controllers/index.ts',
+          pattern: /(export {)/,
+          template:
+            "import {{pascalCase model}}Controller from './{{pascalCase model}}Controller';\n\n$1",
+        },
+        {
+          type: 'modify',
+          path: '../src/repositories/index.ts',
+          pattern: /(export {)/,
+          template:
+            "import {{pascalCase model}}Repository from './{{pascalCase model}}Repository';\n\n$1",
+        },
+        {
+          type: 'append',
+          path: '../src/controllers/index.ts',
+          template: 'export { {{pascalCase model}}Controller };\n',
+          skipIfExists: true,
+        },
+        {
+          type: 'append',
+          path: '../src/repositories/index.ts',
+          template: 'export { {{pascalCase model}}Repository };\n',
+          skipIfExists: true,
+        },
+        {
+          type: 'append',
+          path: '../src/DTOs/index.ts',
+          template:
+            'export { {{pascalCase model}}, Update{{pascalCase model}} };\n',
+          skipIfExists: true,
+        },
+        {
+          type: 'modify',
+          path: '../src/routes/index.ts',
+          pattern: /\n/,
+          template:
+            "\n\nimport {{pascalCase model}}Routes from './{{pascalCase model}}Routes';",
+        },
+        {
+          type: 'modify',
+          path: '../src/routes/index.ts',
+          pattern: /const router = Router\(\);/,
+          template:
+            "const router = Router();\n\nrouter.use('/{{camelCase model}}', {{pascalCase model}}Routes);",
+        },
+      );
+
+      // Ações adicionais para identificador duplo
+      if (data.identifierType === 'Duplo') {
+        actions.push(
+          {
+            type: 'add',
+            path: `../src/controllers/{{pascalCase model}}-{{pascalCase model2}}Controller.ts`,
+            templateFile: `${templatePath}/doubleController.ts.hbs`,
+          },
+          {
+            type: 'add',
+            path: `../src/repositories/{{pascalCase model}}-{{pascalCase model2}}Repository.ts`,
+            templateFile: `${templatePath}/doubleRepository.ts.hbs`,
+          },
+          {
+            type: 'add',
+            path: `../src/routes/{{pascalCase model}}-{{pascalCase model2}}Routes.ts`,
+            templateFile: `${templatePath}/doubleRoutes.ts.hbs`,
+          },
+          {
+            type: 'add',
+            path: `../src/DTOs/{{pascalCase model}}-{{pascalCase model2}}.ts`,
+            templateFile: `${templatePath}/doubleDTO.ts.hbs`,
+          },
+          {
+            type: 'modify',
+            path: '../src/DTOs/index.ts',
+            pattern: /^(import .*;\n)/,
+            template:
+              "import { {{pascalCase model}}, {{pascalCase model2}}, Update{{pascalCase model}} } from './{{pascalCase model}}-{{pascalCase model2}}';\n$1",
+          },
+          {
+            type: 'modify',
+            path: '../src/controllers/index.ts',
+            pattern: /(export {)/,
+            template:
+              "import {{pascalCase model}}-{{pascalCase model2}}Controller from './{{pascalCase model}}-{{pascalCase model2}}Controller';\n\n$1",
+          },
+          {
+            type: 'modify',
+            path: '../src/repositories/index.ts',
+            pattern: /(export {)/,
+            template:
+              "import {{pascalCase model}}-{{pascalCase model2}}Repository from './{{pascalCase model}}-{{pascalCase model2}}Repository';\n\n$1",
+          },
+          {
+            type: 'append',
+            path: '../src/controllers/index.ts',
+            template:
+              'export { {{pascalCase model}}-{{pascalCase model2}}Controller };\n',
+            skipIfExists: true,
+          },
+          {
+            type: 'append',
+            path: '../src/repositories/index.ts',
+            template:
+              'export { {{pascalCase model}}-{{pascalCase model2}}Repository };\n',
+            skipIfExists: true,
+          },
+          {
+            type: 'append',
+            path: '../src/DTOs/index.ts',
+            template:
+              'export { {{pascalCase model}}, {{pascalCase model2}}, Update{{pascalCase model}} };\n',
+            skipIfExists: true,
+          },
+          {
+            type: 'modify',
+            path: '../src/routes/index.ts',
+            pattern: /\n/,
+            template:
+              "\n\nimport {{pascalCase model}}-{{pascalCase model2}}Routes from './{{pascalCase model}}-{{pascalCase model2}}Routes';",
+          },
+          {
+            type: 'modify',
+            path: '../src/routes/index.ts',
+            pattern: /const router = Router\(\);/,
+            template:
+              "const router = Router();\n\nrouter.use('/{{camelCase model}}-{{camelCase model2}}', {{pascalCase model}}-{{pascalCase model2}}Routes);",
+          },
+        );
+      }
+
+      return actions;
+    },
   });
 };
